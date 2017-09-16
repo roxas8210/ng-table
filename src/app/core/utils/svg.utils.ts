33121a81da2011481c1144319480b640d3e69c12
roxas8210 <roxas8210@163.com>
2017-09-16 18:16:22 +0800

    加icon

--- src/app/core/utils/svg.utils.ts
+++ src/app/core/utils/svg.utils.ts
@@ -3,4 +3,6 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 export const loadSvgResources = ( ir: MdIconRegistry, ds: DomSanitizer ) => {
     ir.addSvgIcon('project', ds.bypassSecurityTrustResourceUrl('assets/project.svg'));
+    ir.addSvgIcon('year', ds.bypassSecurityTrustResourceUrl('assets/year.svg'));
+    ir.addSvgIcon('month', ds.bypassSecurityTrustResourceUrl('assets/month.svg'));
 };
